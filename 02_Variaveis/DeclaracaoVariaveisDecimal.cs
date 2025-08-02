c4ed5895ea0cc5ea65c9fc411c8598ac5d15df73
Thiago Freitas <thifreitas26@gmail.com>
2025-08-01 22:12:22 -0300

    Declaração Variaveis Decimal

---
+++ 02_Variaveis/DeclaracaoVariaveisDecimal.cs
@@ -0,0 +1,11 @@
+using System;
+
+class DeclaracaoVariaveisDecimal
+{
+	static void Main(string[] args)
+	{
+		//decimal myMoneyeError = 300.0;
+		decimal myMoneyeCorrect = 300.0m;
+		Console.WriteLine(myMoneyCorrect);
+	}
+}
