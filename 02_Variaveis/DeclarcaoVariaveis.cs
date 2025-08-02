3febbe47cd7746df5ca02b4972821f991ad489f2
Thiago Freitas <thifreitas26@gmail.com>
2025-08-01 21:44:28 -0300

    Declaração de Variaveis

---
+++ 02_Variaveis/DeclarcaoVariaveis.cs
@@ -0,0 +1,30 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+		static void Main()
+		{
+			/*
+			Declaracao de variável
+			*/
+			int a;
+			
+			//Inicialização de valor na variável
+			a = 22;
+			Console.WriteLine(a);
+			
+			//Reatribuição de valor na variável
+			a = 56;
+			Console.WriteLine(a);
+			
+			//Outras formas de declaração de variáveis 
+			
+			//Declarando e inicializando a variável
+			int b =925;
+			Console.WriteLine(b);
+			
+			int y = 10, x = 20;
+			int z = y  + x;
+			Console.WriteLine(z);
+		}
+}
