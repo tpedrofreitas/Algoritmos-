f4d54dbab42fe5e0fe01a44e5fa0a8adbbfcb72f
Thiago Freitas <thifreitas26@gmail.com>
2025-08-04 20:56:32 -0300

    Criação do código de C# para demostrar textos com Placeholder e valores formatados

--- 02_Variaveis/DeclaracaoVariaveisDecimalFormatada.cs
+++ 02_Variaveis/DeclaracaoVariaveisDecimalFormatada.cs
@@ -15,7 +15,16 @@ class DeclacacaoVariavelDecimalFormatado
 		*/
 		Console.WriteLine("Minha quantia = {0:C}",x);
 		Console.WriteLine("Sua quantia = {0:C}",y);
-		
 		Console.WriteLine("Valor de x = {0:F3} e Valor de y = {1:C}",x,y);
+		
+		/*
+		Interpolação de String, só funciona no Net versão 6.0 acima
+		*/
+		/*
+		Console.WriteLine($"Minha quantia = {X:C}",x);
+		Console.WriteLine($"Sua quantia = {Y:C}",y);
+		Console.WriteLine($"Valor de x = {X:F3} e Valor de y = {Y:C}",x,y);
+		*/
+		
 	}
 }
