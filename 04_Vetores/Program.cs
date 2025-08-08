c41e70d9128372ab4d7806df7d35a2995b10ded0
Thiago Freitas <thifreitas26@gmail.com>
2025-08-07 22:21:57 -0300

    Criação de Projeto no Visual Studio

---
+++ 04_Vetores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word !");
+        }
+    }
+}
