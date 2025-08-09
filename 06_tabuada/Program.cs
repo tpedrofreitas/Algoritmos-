579bb81532e2a705dc0bc525a25f9a457cad9a50
Thiago Freitas <thifreitas26@gmail.com>
2025-08-08 22:22:25 -0300

    fazer tabuada

---
+++ 06_tabuada/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06_tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero para ver a tabuada");
+                int numero = int.Parse(Console.ReadLine());
+
+
+            
+
+            Console.WriteLine($"\nTabuada do {numero}:\n");
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+
+            }
+            Console.WriteLine("\nPressione qualquer teclar para sair");
+            Console.ReadKey();
+           
+
+        }
+    }
+}
