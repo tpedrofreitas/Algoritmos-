bd29495b6f2d2d4414e8d8a5a51bd38edaf8fc27
Thiago Freitas <thifreitas26@gmail.com>
2025-08-11 20:09:53 -0300

    alterei o Tryparse

--- 06_tabuada/Program.cs
+++ 06_tabuada/Program.cs
@@ -4,25 +4,37 @@ namespace _06_tabuada
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
-            Console.WriteLine("Digite um numero para ver a tabuada");
-                int numero = int.Parse(Console.ReadLine());
+            int multiplicando = 0, produto;
+            bool digitacaoValida = false;
 
+            // ! negação: inverte o valor booleano
+            // !false = true
+            // !true = false
+            // while ( digitacaoValida == false )
 
-            
+            while ( !digitacaoValida )
+              {
+                Console.WriteLine("Digite um numero inteiro");
+                 digitacaoValida = int.TryParse(Console.ReadLine(), out multiplicando );
+              }
 
-            Console.WriteLine($"\nTabuada do {numero}:\n");
+            // Console.WriteLine("Tabuada do {0}",multiplicando;
+            //Interpolação de String $
 
-            for (int i = 1; i < 10; i++)
+            Console.WriteLine($"Tabuada do {multiplicando}");
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
             {
-                Console.WriteLine($"{numero} x {i} = {numero * i}");
+                produto = multiplicando * multiplicador;
+                //Console.Write("{0} x {1} = {2} ", multiplicando,multiplicador, produto)
 
+                Console.WriteLine($"{multiplicando} x {multiplicador} = {produto} ");
             }
-            Console.WriteLine("\nPressione qualquer teclar para sair");
-            Console.ReadKey();
+       }
+    }
+ }          
            
 
-        }
-    }
-}
+        
