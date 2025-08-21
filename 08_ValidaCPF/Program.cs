c2891d1b9955d50eed244f32b368c0b507c3417f
Thiago Freitas <thifreitas26@gmail.com>
2025-08-21 20:52:56 -0300

    Validação de CPF

---
+++ 08_ValidaCPF/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace _08_ValidaCPF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu CPF (somente numeros): ");
+            string CPF = Console.ReadLine();
+            
+
+            CPF = Regex.Replace(CPF, "[^ 0-9]", "");
+            if (CPF.Length != 11)
+            {
+                Console.WriteLine("CPF inválido! Deve ter 11 numeros.");
+                return;
+            }
+            bool todosIguais = true;
+            for (int i = 1; i < CPF.Length; i++)
+            {
+                if (CPF[i] != CPF[0])
+
+                {
+                    todosIguais = false;
+                    break;
+                }
+            }
+            if (todosIguais)
+            {
+                Console.WriteLine("CPF inválido!Não pode ter  Todos os números são iguais.");
+                return;
+            }
+            Console.WriteLine("CPF aceito: " + CPF);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+    
+
